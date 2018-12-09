ce678aa811474971a71a1d283aae46553c825488
Ayushi <ayushi0430@outlook.com>
2018-12-09 13:18:13 +0530

    Move db search to handlers

--- handlers.go
+++ handlers.go
@@ -4,15 +4,41 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-
+    "github.com/blevesearch/bleve"
 	"github.com/gorilla/mux"
 )
 
-func GetIssues(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	queryParam := r.Form.Get("query")
-	fmt.Println(queryParam)
-	json.NewEncoder(w).Encode(issues)
+func GetIssues(w http.ResponseWriter, r *http.Request) {    
+	// r.ParseForm()
+	// queryParam := r.Form.Get("query")
+    params := mux.Vars(r)
+    index, _ := bleve.Open("test.bleve")
+
+    query := bleve.NewQueryStringQuery(params["query"])
+    search := bleve.NewSearchRequest(query)
+    search.Fields = []string{"*"}
+	searchResults, err := index.Search(search)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+    fmt.Println(searchResults)
+    var issues []Issue
+    for _, res := range searchResults.Hits {
+        fmt.Println(res)
+        issue := Issue{
+            MssgShort : res.Fields["mssgShort"].(string),
+            Status : res.Fields["status"].(string),
+            Area : res.Fields["area"].(string),
+            Address : res.Fields["area"].(string),
+            PhoneNo : res.Fields["phoneNo"].(string),
+            DisasterType : res.Fields["disasterType"].(string),
+            SquashedMssgs : res.Fields["squashedMssgs"].([]string),
+            Acknowledgement : res.Fields["acknowledgement"].(bool),
+        }
+        issues = append(issues, issue)
+    }
+    json.NewEncoder(w).Encode(issues)
 }
 
 func GetIssue(w http.ResponseWriter, r *http.Request) {
